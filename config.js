eaa80312a84d7cb620a358affde36d7f61ae9495
adji142 <prasetyoajiw@gmail.com>
2018-10-29 19:49:59 +0700

    3

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-export const facebook ={
+const facebook ={
     clientID: '352575901957767',
     clientSecret: '584261e1c89439a50a2d858ee76f9860',
     callbackURL:'https://backendkopdar.herokuapp.com/auth/facebook/callback',
     profileFields: ['id','token' ,'name', 'displayName', 'picture', 'email'],
 };
-export const google = {
+const google = {
     clientID: '604043265630-k9pvklmbh5fktmfg7jt3lcl6f4ruq5ul.apps.googleusercontent.com',
     clientSecret: 'ig1SG1M2PwJPRD_SIPJiCecj',
     callbackURL: 'https://backendkopdar.herokuapp.com/auth/google/callback',
   };
-
+export {google};
 // const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 // module.exports = (passport) => {
 //     passport.serializeUser((user, done) => {
