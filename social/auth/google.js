f79c0dcaf1bcab9a80195e2b5a3d8446a4311ebb
adji142 <prasetyoajiw@gmail.com>
2018-11-10 22:51:47 +0700

    g

--- social/auth/google.js
+++ social/auth/google.js
@@ -1,6 +1,6 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var User = require('../models/User');
+var user = require('../models/User');
 
 passport.use(new GoogleStrategy({
     clientID: "604043265630-k9pvklmbh5fktmfg7jt3lcl6f4ruq5ul.apps.googleusercontent.com",
@@ -8,7 +8,7 @@ passport.use(new GoogleStrategy({
     callbackURL: "https://backendkopdar.herokuapp.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id }, function (err, user) {
+       user.FindOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id }, function (err, user) {
          return done(err, user);
        });
   }
