898c9a7a4a17d655a8ae7ea49774866980b843e0
adji142 <prasetyoajiw@gmail.com>
2018-11-10 22:43:39 +0700

    bug fux

--- social/auth/google.js
+++ social/auth/google.js
@@ -3,9 +3,9 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var User = require('../models/User');
 
 passport.use(new GoogleStrategy({
-    clientID: "604043265630-jp3qfd0fnrc488q06q4q3043l49r008h.apps.googleusercontent.com",
-    clientSecret: "2nJsgCOYpRjXTc2W7WiGDCcX",
-    callbackURL: "https://backendkopdar.herokuapp.com/auth/facebook/callback"
+    clientID: "604043265630-k9pvklmbh5fktmfg7jt3lcl6f4ruq5ul.apps.googleusercontent.com",
+    clientSecret: "t4h4v4Ajo3BLPfWgjh4IE7wd",
+    callbackURL: "https://backendkopdar.herokuapp.com/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ userid: profile.id }, { name: profile.displayName,userid: profile.id }, function (err, user) {
